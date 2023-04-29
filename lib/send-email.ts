9d6b7b37c7363c9098e79e23e59e12e8aca6c4f3
rcabre95 <raphael.cabrera111@gmail.com>
2023-04-28 19:09:11 -0500

    added await to /lib/send-email.ts

--- lib/send-email.ts
+++ lib/send-email.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function sendEmail(name: string, email: string, subject: string, message: string) {
-    return axios({
+    return await axios({
         method: 'POST',
         url: `${process.env.NEXT_PUBLIC_HOME_URL}/api/send-mail`,
         data: {
