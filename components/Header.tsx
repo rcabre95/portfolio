30b4017585c7e22c3593ae4a2959ba8ed0a3ef94
rcabre95 <raphael.cabrera111@gmail.com>
2023-05-15 21:30:09 -0500

    brought header to top layer

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ export default function Header({ section, homeRef, aboutRef, projectsRef, contac
     };
 
     return (
-        <div className={`h-16 bg-myBlue w-full sticky top-0 bg-opacity-90 shadow-sm backdrop-filter backdrop-blur-sm flex justify-around font-bold md:justify-end z-30`}>
+        <div className={`h-16 bg-myBlue w-full sticky top-0 bg-opacity-90 shadow-sm backdrop-filter backdrop-blur-sm flex justify-around font-bold md:justify-end z-50`}>
             <button onClick={scrollToHome} className={`md:mx-12 transition-colors hover:text-yellow-400 ${section === "home" ? "text-yellow-400" : "text-myGrey"}`}>Home</button>
             <button onClick={scrollToAbout} className={`md:mx-12 transition-colors hover:text-yellow-400 ${section === "about" ? "text-yellow-400" : "text-myGrey"}`}>About</button>
             <button onClick={scrollToProjects} className={`md:mx-12 transition-colors hover:text-yellow-400 ${section === "projects" ? "text-yellow-400" : "text-myGrey"}`}>Projects</button>
